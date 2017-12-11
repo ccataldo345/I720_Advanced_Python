2984ab974de207ecf4666dd5eed4e1bb5d94a2e4
Christian Cataldo <christian.cataldo@itcollege.ee>
2017-12-11 17:14:07 +0200

    print max value

--- project.py
+++ project.py
@@ -1,20 +1,35 @@
 from datetime import datetime
 import requests
 import json
+import numpy
 
 # convert a timestamp to python native datetime object
-timestamp = 1512864000
+timestamp = 1512604800
 d = datetime.fromtimestamp(timestamp)
-print(d)
+# print(d)
+
 
 r = requests.get("https://api.blockchain.info/charts/market-price?format=json")
 data = r.json()
 
 #print(data['values'])
 
+list_dates = []
+list_values =[]
+
 #print dates:
 for num in data['values']:
-	date = datetime.fromtimestamp(num['x'])
+	#date = datetime.fromtimestamp(num['x'])
+	date = num['x']
 	value = num['y']
-	print(date, "; ", value)
+	list_dates.append(date)
+	list_values.append(value) 
 
+#print(date, "; ", value)
+#print(list_dates)
+
+list = list(zip(list_values, list_dates))
+
+# print max value
+max_list = max(list)
+print ("Max value USD: ", max_list[0], " on date: ", datetime.fromtimestamp(max_list[1]))

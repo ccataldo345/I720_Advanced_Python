fde1dd81ee9b847a1b92131458babaa04e64df5d
Christian Cataldo <christian.cataldo@itcollege.ee>
2017-12-11 16:04:00 +0200

    print values

--- project.py
+++ project.py
@@ -1,12 +1,20 @@
 from datetime import datetime
 import requests
+import json
 
 # convert a timestamp to python native datetime object
 timestamp = 1512864000
 d = datetime.fromtimestamp(timestamp)
 print(d)
 
-
 r = requests.get("https://api.blockchain.info/charts/market-price?format=json")
-values = r.json()
-print(values)
+data = r.json()
+
+#print(data['values'])
+
+#print dates:
+for num in data['values']:
+	date = datetime.fromtimestamp(num['x'])
+	value = num['y']
+	print(date, "; ", value)
+

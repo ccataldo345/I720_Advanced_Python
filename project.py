3c18d563245048d818db137aefabd0d4ea012f4d
ccataldo345 <post345@outlook.com>
2017-12-26 12:39:26 +0200

    Add max increase and decrease

--- project.py
+++ project.py
@@ -41,43 +41,48 @@ for num in data['values']:
 
 list = list(zip(list_values, list_dates))
 
+'''
+# print all values, dates
+for i in range(len(list) ):
+	print(list[i][0], ", ", datetime.fromtimestamp(list[i][1]))
+'''
+
 # print max value
 max_list = max(list)
+max_list_value = max_list[0]
+max_list_date = max_list[1]
 print ()
-print ("Max value USD: ", max_list[0], " on date: ", datetime.fromtimestamp(max_list[1]))
-print ()
-
-# print greatest percent increase over the previous day
-print ("day 0: ", list[0][0])
-print ("day 1: ", list[1][0])
-print ("diff: ", list[1][0] - list[0][0])
-max_diff = round((list[1][0] - list[0][0])/list[0][0], 5)
-print("diff %: ", max_diff, "%")
+print ("Max value USD: ", max_list_value, " on date: ", datetime.fromtimestamp(max_list_date))
 print ()
 
 
-min = list[0][0]
-max = list[0][0]
-max_diff = -1
+# print max increase
+max_change_increase_value = 0
+max_change_increase_date = None
 
-
-count=0
-max = 0
-'''
-for value, date in list:
-	count = count + 1
-	diff = value[1] - value
-	
-	
-	# if (max_i > max):
-		# max = max_i
+for i in range(len(list) -1):
+	previous = list[i][0]
+	current = list[i+1][0]
+	change = (current - previous) / previous * 100 
+	if change > max_change_increase_value:
+		max_change_increase_value = change
+		max_change_increase_date = list[i+1][1]
 		
-	print(diff)
-'''	
+print ("Max increase: ", round(max_change_increase_value, 2), "%,  on date: ", datetime.fromtimestamp(max_change_increase_date))
+print ()
 
 
-print (type(list))
-print (len(list))
-value = list[364][0]
-date = list[364][1]
-print (value, " on date: ", datetime.fromtimestamp(date))
+# print max decrease
+max_change_decrease_value = 0
+max_change_decrease_date = None
+
+for i in range(len(list) -1):
+	previous = list[i][0]
+	current = list[i+1][0]
+	change = (current - previous) / previous * 100
+	if change < max_change_decrease_value:
+		max_change_decrease_value = change
+		max_change_decrease_date = list[i+1][1]
+		
+print ("Max decrease: ", round(max_change_decrease_value, 2), "%, on date: ", datetime.fromtimestamp(max_change_decrease_date))
+print ()

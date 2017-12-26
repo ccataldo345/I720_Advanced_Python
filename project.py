d96e6ab8c66da79bf189385a9bffce9a2caa556c
ccataldo345 <post345@outlook.com>
2017-12-26 11:25:23 +0200

    Add previuos day difference

--- project.py
+++ project.py
@@ -1,7 +1,11 @@
 from datetime import datetime
 import requests
 import json
-import numpy
+
+# from django.http import HttpResponse
+# import pandas as pd
+
+
 
 # convert a timestamp to python native datetime object
 timestamp = 1512604800
@@ -11,8 +15,13 @@ d = datetime.fromtimestamp(timestamp)
 
 r = requests.get("https://api.blockchain.info/charts/market-price?format=json")
 data = r.json()
-
-#print(data['values'])
+'''
+date = data['values']['x']
+price = data['values']['y']
+resDate = HttpResponse(date)
+resPrice = HttpResponse(price)
+print(resDate, resPrice)
+'''
 
 list_dates = []
 list_values =[]
@@ -25,11 +34,50 @@ for num in data['values']:
 	list_dates.append(date)
 	list_values.append(value) 
 
-#print(date, "; ", value)
-#print(list_dates)
+	#print(date, "; ", value)
+
+#print(list_values)
+
 
 list = list(zip(list_values, list_dates))
 
 # print max value
 max_list = max(list)
+print ()
 print ("Max value USD: ", max_list[0], " on date: ", datetime.fromtimestamp(max_list[1]))
+print ()
+
+# print greatest percent increase over the previous day
+print ("day 0: ", list[0][0])
+print ("day 1: ", list[1][0])
+print ("diff: ", list[1][0] - list[0][0])
+max_diff = round((list[1][0] - list[0][0])/list[0][0], 5)
+print("diff %: ", max_diff, "%")
+print ()
+
+
+min = list[0][0]
+max = list[0][0]
+max_diff = -1
+
+
+count=0
+max = 0
+'''
+for value, date in list:
+	count = count + 1
+	diff = value[1] - value
+	
+	
+	# if (max_i > max):
+		# max = max_i
+		
+	print(diff)
+'''	
+
+
+print (type(list))
+print (len(list))
+value = list[364][0]
+date = list[364][1]
+print (value, " on date: ", datetime.fromtimestamp(date))
